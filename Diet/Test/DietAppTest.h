dd6922c9ec9a32fdbed91a5df6885e1fdfd5b275
Matthew <67207999+matthewJ1981@users.noreply.github.com>
2021-07-31 22:18:13 -0400

    Some dietapp tests

--- Diet/Test/DietAppTest.h
+++ Diet/Test/DietAppTest.h
@@ -1,18 +1,28 @@
 #pragma once
 #include <gtest/gtest.h>
 #include "..\DietApp.h"
-#include "..\Util.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include "..\DietAppImplementation.h"
 #include <fstream>
 #include <iostream>
 
+using namespace boost::gregorian;
+using namespace boost::posix_time;
+using namespace Diet::Internal;
 
 class DietAppTest : public ::testing::Test
 {
 protected:
 	void SetUp() override
-	{	
-		;
+	{
+		DietAppImplementation::consumed = consumedDef;
+		DietAppImplementation::favorites = favoritesDef;
+		DietAppImplementation::total = totalDef;
+
+		DietAppImplementation::calorieMax = calorieMaxDef;
+		DietAppImplementation::prevRunDate = prevRunDateDef;
+		DietAppImplementation::currRunDate = currRunDateDef;
+		DietAppImplementation::startHour = startHourDef;;
 	}
 
 	void TearDown() override
@@ -20,6 +30,24 @@ protected:
 		;
 	}
 
-	Diet::Internal::DietAppImplementation di;
+	static const std::vector<std::pair<Diet::FoodItem, float>> consumedDef;
+	static const std::vector<Diet::FoodItem> favoritesDef;
+	static const Diet::FoodItem totalDef;
+	static constexpr float totFatMaxDef = 75.0f;
+	static constexpr float totSatFatMaxDef = 20.0f;
+	static constexpr float totCholMaxDef = 300.0f;
+	static constexpr float totSodMaxDef = 2400.0f;
+	static constexpr float totCarbMaxDef = 250.0f;
+	static constexpr float totFibreMaxDef = 28.0f;
+	static constexpr float totProteinMaxDef = 50.0f;
+
+	static constexpr float calorieMaxDef = 2000.0f;
+	static const std::string consumedFileDef;
+	static const std::string favoritesFileDef;
+	static const std::string configFileDef;
+	static const std::string totalsFileDef;
+	static const boost::gregorian::date prevRunDateDef;
+	static const boost::gregorian::date currRunDateDef;
+	static constexpr _int64 startHourDef = 0;
 };
 
--- Diet/Test/Test_DietApp.cpp
+++ Diet/Test/Test_DietApp.cpp
@@ -4,6 +4,19 @@
 
 using ::testing::Eq;
 
+//using namespace Diet::Internal;
+
+const std::vector<std::pair<Diet::FoodItem, float>> DietAppTest::consumedDef{};
+const std::vector<Diet::FoodItem> DietAppTest::favoritesDef{};
+const Diet::FoodItem DietAppTest::totalDef{ "Total" };
+const std::string DietAppTest::consumedFileDef = "Data/consumed.txt";
+const std::string DietAppTest::favoritesFileDef = "Data/favorites.txt";
+const std::string DietAppTest::configFileDef = "Data/config.txt";
+const std::string DietAppTest::totalsFileDef = "Data/totals.txt";
+const date DietAppTest::prevRunDateDef = date(not_a_date_time);
+const date DietAppTest::currRunDateDef = date(not_a_date_time);
+
+
 TEST_F(DietAppTest, Go)
 {
 	FAIL();
@@ -54,36 +67,116 @@ TEST_F(DietAppTest, Serialize)
 }
 TEST_F(DietAppTest, Deserialize)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	DietAppImplementation::consumed.push_back({ {"Test"}, 2 });
+	DietAppImplementation::favorites.push_back( {"Test" });
+	DietAppImplementation::calorieMax = 500.0f;
+	DietAppImplementation::prevRunDate = date(not_a_date_time);
+
+	DietAppImplementation::Serialize();
+	std::vector<std::pair<Diet::FoodItem, float>> consumedTest = DietAppImplementation::consumed;
+	std::vector<Diet::FoodItem> favoritesTest = DietAppImplementation::favorites;
+	float calorieMaxTest = DietAppImplementation::calorieMax;
+	DietAppImplementation::consumed.clear();
+	DietAppImplementation::favorites.clear();
+	DietAppImplementation::calorieMax = 0.0f;
+	DietAppImplementation::prevRunDate = date(not_a_date_time);
+
+	DietAppImplementation::Deserialize();
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(calorieMaxTest));
+	EXPECT_THAT(DietAppImplementation::consumed, Eq(consumedTest));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesTest));
+	EXPECT_THAT(DietAppImplementation::total, Eq(totalDef));
+	EXPECT_THAT(DietAppImplementation::prevRunDate, Eq(prevRunDateDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
 }
 TEST_F(DietAppTest, WriteTotal)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	std::ofstream out(totalsFileDef);
+	out.close();
+	DietAppImplementation::WriteTotal();
+	std::ifstream in(totalsFileDef);
+	date d;
+	Diet::FoodItem f;
+	in >> d;
+	f.Deserialize(in);
+	EXPECT_THAT(d, Eq(prevRunDateDef));
+	EXPECT_THAT(f, totalDef);
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(calorieMaxDef));
+	EXPECT_THAT(DietAppImplementation::consumed, Eq(consumedDef));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
 }
 TEST_F(DietAppTest, ReadTotal)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	DietAppImplementation::ReadTotal();
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(calorieMaxDef));
+	EXPECT_THAT(DietAppImplementation::consumed, Eq(consumedDef));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesDef));
+	EXPECT_THAT(DietAppImplementation::total, Eq(totalDef));
+	EXPECT_THAT(DietAppImplementation::prevRunDate, Eq(prevRunDateDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
 }
 TEST_F(DietAppTest, ClearConsumedFile)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	DietAppImplementation::consumed.push_back({ { "Food"}, 1 });
+	DietAppImplementation::Serialize();
+	std::ifstream in(consumedFileDef);
+	if (!in)
+		abort();
+	std::string before;
+	in >> before;
+	in.close();
+	DietAppImplementation::ClearConsumedFile();
+	in.open(consumedFileDef);
+	if (!in)
+		abort();
+	std::string after;
+	in >> after;
+	EXPECT_TRUE(!before.empty());
+	EXPECT_TRUE(after.empty());
+
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(calorieMaxDef));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesDef));
+	EXPECT_THAT(DietAppImplementation::total, Eq(totalDef));
+	EXPECT_THAT(DietAppImplementation::prevRunDate, Eq(prevRunDateDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
+
 }
 TEST_F(DietAppTest, History)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	DietAppImplementation::History();
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(calorieMaxDef));
+	EXPECT_THAT(DietAppImplementation::consumed, Eq(consumedDef));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesDef));
+	EXPECT_THAT(DietAppImplementation::total, Eq(totalDef));
+	EXPECT_THAT(DietAppImplementation::prevRunDate, Eq(prevRunDateDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
 }
 TEST_F(DietAppTest, Options)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	DietAppImplementation::Options();
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(calorieMaxDef));
+	EXPECT_THAT(DietAppImplementation::consumed, Eq(consumedDef));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesDef));
+	EXPECT_THAT(DietAppImplementation::total, Eq(totalDef));
+	EXPECT_THAT(DietAppImplementation::prevRunDate, Eq(prevRunDateDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
 }
 TEST_F(DietAppTest, SetCalorieMax)
 {
-	FAIL();
-	//EXPECT_THAT(0, Eq(0));
+	float newCalMax = 3000.0f;
+	DietAppImplementation::SetCalorieMax(newCalMax);
+	EXPECT_THAT(DietAppImplementation::calorieMax, Eq(newCalMax));
+	EXPECT_THAT(DietAppImplementation::consumed, Eq(consumedDef));
+	EXPECT_THAT(DietAppImplementation::favorites, Eq(favoritesDef));
+	EXPECT_THAT(DietAppImplementation::total, Eq(totalDef));
+	EXPECT_THAT(DietAppImplementation::prevRunDate, Eq(prevRunDateDef));
+	EXPECT_THAT(DietAppImplementation::currRunDate, Eq(currRunDateDef));
+	EXPECT_THAT(DietAppImplementation::startHour, Eq(startHourDef));
 }
